b0b1f51869e0301ee0945d51743daefdb3c5e0aa
agusanggarwanto <agusanggarwanto@gmail.com>
2024-08-01 11:03:33 +0700

    populate homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,38 @@
 import Link from "next/link";
+import styles from "./page.module.css"
 
 export default function Home() {
     return (
-      <main>
-        <h1 style={{ color: 'white', textAlign: 'center' }}>
-          Waktunya dimulai!
-        </h1>
-        <p><Link href={"/hidangan"}>Hidangan</Link></p>
-        <p><Link href={"/hidangan/shares"}>Bagikan Hidangan</Link></p>
-        <p><Link href={"/komunitas"}>Komunitas</Link></p>
-      </main>
+      <>
+        <header className={styles.header}>
+          <div className={styles.slideshow}></div>
+          <div>
+            <div className={styles.hero}>
+              <h1>Cita Rasa untuk penikmat cita rasa Nusantara</h1>
+              <p>Nikmati dan bagikan cita rasa Nusantara</p>
+            </div>
+            <div className={styles.cta}>
+              <Link href={"/komunitas"}>Bergabung Komunitas</Link>
+              <Link href={"/hidangan"}>Temukan Hidangan</Link>
+            </div>
+          </div>
+        </header>
+        <main>
+          <section className={styles.section}>
+            <h2>Bagaimana cara kerjanya?</h2>
+            <p>Cita Rasa adalah platform di mana para pecinta makanan berbagi resep masakan favoritnya kepada seluruh duni. Ini tempat di mana kita bisa menemukan resep baru dan terhubung dengan orang lain.</p>
+            <p>
+              Cita Rasa adalah tempat untuk menemukan resep baru dan terhubung dengan orang lain.
+            </p>
+          </section>
+          <section className={styles.section}>
+          <h2>Bagaimana cara kerjanya?</h2>
+            <p>Cita Rasa adalah platform di mana para pecinta makanan berbagi resep masakan favoritnya kepada seluruh duni. Ini tempat di mana kita bisa menemukan resep baru dan terhubung dengan orang lain.</p>
+            <p>
+              Cita Rasa adalah tempat untuk menemukan resep baru dan terhubung dengan orang lain.
+            </p>
+          </section>
+        </main>
+      </>
     );
   }
